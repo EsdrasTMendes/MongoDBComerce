2decfb55ce17c4297fe6ff368ff6aaa27d36476d
Esdras <esdrastmendes@gmail.com>
2022-08-16 00:28:17 -0300

    fix: correção lint, agora vai!

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,3 @@
 db.produtos
-  .find({ vendidos: { $lt: 100, $gt: 50 } }, { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 });
+  .find({ vendidos: { $lt: 100, $gt: 50 } }, { nome: 1, vendidos: 1, _id: 0 })
+    .sort({ vendidos: 1 });
